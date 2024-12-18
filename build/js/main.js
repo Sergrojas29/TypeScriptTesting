3d364e31ab1aeca52a332f0fcb6eb04a81d6e74c
Sergio RA <Sergrojas29@gmail.com>
2024-12-18 02:15:36 -0500

    test Classes and makers

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,65 @@
 "use strict";
-let username = 'Dave';
-let a = 12;
-let b = 6;
-let c = 2;
-console.log(a / b);
-console.log(c * b);
+class Game {
+    constructor() {
+        this.teams = [];
+        this.round = 0;
+        this.roundTime = 30;
+        this.activeCards = [];
+        this.usedCards = [];
+    }
+    addTeam(newteam) {
+        this.teams.push(newteam);
+    }
+    creatCard(title, description, point = 1) {
+        const ret = {
+            id: ++Game.numOfCards,
+            title: title,
+            description: description,
+            point: point,
+        };
+        return ret;
+    }
+    addCard(title, description, point) {
+        const ret = this.creatCard(title, description, point);
+        this.activeCards.push(ret);
+    }
+    removeCardByID(cardID) {
+        const cardIndex = this.activeCards.findIndex((card) => card.id === cardID);
+        try {
+            const [cardRemoved] = this.activeCards.splice(cardIndex, 1);
+            this.usedCards.push(cardRemoved);
+        }
+        catch (error) {
+            console.warn(`Card with ID ${cardID} not found`);
+        }
+    }
+    getActiveCards() {
+        return this.activeCards;
+    }
+    getUsedCards() {
+        return this.usedCards;
+    }
+}
+Game.numOfCards = 0;
+class Team {
+    constructor() {
+        this.totalPoint = 0;
+        this.cards = [];
+        this.id = ++Team.numberOfteam;
+    }
+    updateScore(acuiredCard) {
+        this.totalPoint += acuiredCard.point;
+    }
+}
+Team.numberOfteam = 0;
+const testGame = new Game();
+const team1 = new Team();
+testGame.addCard("Kraken", "Monster of the Sea", 3);
+testGame.addCard("The Nanny", "Funny lady from a sitcom");
+testGame.addTeam(team1);
+console.log(testGame.getActiveCards());
+console.log(team1.totalPoint);
+team1.updateScore(testGame.activeCards[0]);
+console.log(team1.totalPoint);
+testGame.removeCardByID(2);
+console.log(testGame.activeCards);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,87 @@
-let username = 'Dave';
-let a: number = 12;
-let b: number = 6;
-let c: number = 2;
+class Game {
+    static numOfCards = 0;
+    public teams: Team[] = [];
+    public round: number = 0;
+    private roundTime: number = 30;
+    activeCards: Card[] = [];
+    usedCards: Card[] = []
+    constructor() {
+    }
+    addTeam(newteam: Team): void{
+        this.teams.push(newteam)
+    }
+    creatCard(title:string, description?: string, point:number=1):Card {
+        const ret: Card = {
+            id: ++Game.numOfCards,
+            title: title,
+            description: description,
+            point: point,
+        } 
+        return ret;
+    }
+    addCard(title:string, description?: string, point?:number): void {
+        const ret = this.creatCard(title, description, point);
+        this.activeCards.push(ret);
+    }
+    removeCardByID(cardID: number): void {
+        const cardIndex = this.activeCards.findIndex((card) => card.id === cardID);
+        try {
+            const [cardRemoved] = this.activeCards.splice(cardIndex, 1);
+            this.usedCards.push(cardRemoved)
+            
+        } catch (error) {
+            console.warn(`Card with ID ${cardID} not found`)
+        }
+        
+    }
+    getActiveCards(): Card[] {
+        return this.activeCards;
+    }
+    getUsedCards(): Card[] {
+        return this.usedCards;
+    }
+}
 
-console.log(a/b)
+class Team {
+    static numberOfteam: number = 0; 
+    public id: number;
+    public totalPoint: number =0;
+    cards:Card[] = [];
+    constructor() {
+        this.id = ++Team.numberOfteam;
+    }
+    updateScore(acuiredCard: Card):void{
+        this.totalPoint += acuiredCard.point
+    }
 
-console.log(c * b)
+}
+
+
+type Card = {
+    [index: string] :string| number | undefined
+    id: number ;
+    title: string;
+    description?: string;
+    point: number;
+}
+
+
+const testGame:Game = new Game();
+
+const team1:Team = new Team();
+
+testGame.addCard("Kraken", "Monster of the Sea",3)
+testGame.addCard("The Nanny", "Funny lady from a sitcom")
+
+testGame.addTeam(team1);
+
+console.log(testGame.getActiveCards())
+
+console.log(team1.totalPoint)
+team1.updateScore(testGame.activeCards[0])
+console.log(team1.totalPoint)
+
+testGame.removeCardByID(2)
+
+console.log(testGame.activeCards)
+
